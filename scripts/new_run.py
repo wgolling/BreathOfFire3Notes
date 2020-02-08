4df16000eba554454d3c98d7552f9c1d2622c47d
William Gollinger <wgolling@gmail.com>
2020-02-08 16:57:34 -0500

    move RunFolderMaker to a module

--- scripts/new_run.py
+++ scripts/new_run.py
@@ -1,59 +1,15 @@
-## must be called from notes folder for the paths to work out.
-
 from pathlib import Path
 from objects.newrun import RunFolderMaker
-
 
 #
 # Main method.
 #
 def main():
 
-  path_to_script = Path(__file__).absolute()
-  # print(path_to_script)
-  path_to_runs_folder = path_to_script.parents[1]
-  # print(path_to_runs_folder)
-  base_path = path_to_runs_folder
+  # Get the path to the parent of the folder containing this script file.
+  path_to_runs_folder = Path(__file__).absolute().parents[1]
 
-  # #
-  # # Testing constructor
-  # #
- 
-  # # invalid base_path input
-  # try:
-  #   RunFolderMaker(7)
-  # except Exception as e:
-  #   print("Error: " + str(e))
-  # try:
-  #   RunFolderMaker(base_path / 'foo')
-  # except Exception as e:
-  #   print("Error: " + str(e))
-  # try:
-  #   RunFolderMaker(base_path / 'run_dummy.txt')
-  # except Exception as e:
-  #   print("Error: " + str(e))
-
-  # # invalid template_path input
-  # try:
-  #   RunFolderMaker(base_path, template_path=7)
-  # except Exception as e:
-  #   print("Error: " + str(e))
-  # try:
-  #   RunFolderMaker(base_path, template_path=base_path / 'foo')
-  # except Exception as e:
-  #   print("Error: " + str(e))
-  # try:
-  #   RunFolderMaker(base_path, template_path=base_path / 'run_dummy.txt')
-  # except Exception as e:
-  #   print("Error: " + str(e))
-
-
-  #
-  # Script
-  #
-
-  # Create RunFolderMaker object, and setup folder.
-  rfm = RunFolderMaker(base_path)
+  rfm = RunFolderMaker(path_to_runs_folder)
   rfm.setup_folder()
 
 if __name__ == '__main__':
