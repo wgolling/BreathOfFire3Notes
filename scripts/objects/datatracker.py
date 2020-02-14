8c8c9583ae9d8c37d0fcdc36e23c920e0535b6b0
William Gollinger <wgolling@gmail.com>
2020-02-13 23:25:43 -0500

    add Character enum and starting levels

--- scripts/objects/datatracker.py
+++ scripts/objects/datatracker.py
@@ -1,3 +1,25 @@
+from enum import Enum, auto
+
+class Character(Enum):
+  RYU = auto()
+  REI = auto()
+  TEEPO = auto()
+  NINA = auto()
+  MOMO = auto()
+  PECO = auto()
+  GARR = auto()
+
+STARTING_LEVELS = {
+  Character.RYU: 1,
+  Character.REI: 5,
+  Character.TEEPO: 1,
+  Character.NINA: 5,
+  Character.MOMO: 10,
+  Character.PECO: 1,
+  Character.GARR: 13,
+}
+
+
 class DataTracker:
 
   def __init__(self):
